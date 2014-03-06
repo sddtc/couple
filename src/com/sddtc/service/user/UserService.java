b17087be10936c7f5ae67f106826f63a3aaf8717
sddtc <changhbaga@gmail.com>
2014-03-07 00:11:23 +0800

    update

--- src/com/sddtc/service/user/UserService.java
+++ src/com/sddtc/service/user/UserService.java
@@ -1,14 +1,26 @@
 package com.sddtc.service.user;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.sddtc.model.User;
+import com.sddtc.utils.param.UserParam;
 
 /**
  * @author sddtc
  *
  */
 public interface UserService {
-	
-	public void addUser(User user);
-	
-	public User getUser(int id);
+    /**
+     * 添加用户
+     * @param user
+     */
+    @Transactional
+    public void addUser(User user);
+    
+    /**
+     * 根据查询条件获取用户
+     * @param param 查询条件
+     * @return
+     */
+    public User getUser(UserParam param);
 }
--- src/com/sddtc/service/user/impl/UserServiceImpl.java
+++ src/com/sddtc/service/user/impl/UserServiceImpl.java
@@ -1,35 +1,49 @@
 package com.sddtc.service.user.impl;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sddtc.mapper.UserMapper;
 import com.sddtc.model.User;
 import com.sddtc.service.user.UserService;
+import com.sddtc.utils.param.UserParam;
 
 /**
  * @author sddtc
  *
  */
 @Service
+@Transactional
 public class UserServiceImpl implements UserService{
-	
-	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
-	
-	@Resource
+    
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+    
+    @Resource
     private UserMapper userMapper;
+    
+    public void addUser(User user) {
+        UserParam param = new UserParam(user.getLoginId());
+        User exist = getUser(param);
 
-	@Override
-	public void addUser(User user) {
-        userMapper.add(user);
-        logger.info("添加用户成功{}", user.toString());
-	}
+        if(null != exist) {
+           exist.setPassword(user.getPassword());
+           userMapper.update(exist);
+           logger.info("更新用户成功", user.toString());
+        } else {
+            user.setCreate_date(new Date());
+            userMapper.add(user);
+            logger.debug("用户添加成功",user.toString());
+        }
+    }
 
-	@Override
-	public User getUser(int id) {
-		return null;
-	}
+    public User getUser(UserParam param) {
+        User exist = userMapper.get(param);
+        return exist;
+    }
 }
