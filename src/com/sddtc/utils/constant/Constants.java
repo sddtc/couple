c70b160b5ff16b2bdf70657e809eeeef20dafcb7
sddtc <changhbaga@gmail.com>
2014-04-09 00:02:25 +0800

    some constants

---
+++ src/com/sddtc/utils/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.sddtc.utils.constant;
+
+/**
+ * @author sddtc
+ * 公用常量
+ */
+public class Constants {
+	
+	public static final int VALID = 1;
+	
+	public static final int INVALID = 2;
+}
