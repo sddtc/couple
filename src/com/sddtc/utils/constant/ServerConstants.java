6a8ed43a077e41f5c333e9c27f6f6ffd4919d9d0
sddtc <changhbaga@gmail.com>
2014-04-08 23:39:22 +0800

    add config

---
+++ src/com/sddtc/utils/constant/ServerConstants.java
@@ -0,0 +1,12 @@
+package com.sddtc.utils.constant;
+
+/**
+ * @author sddtc
+ * 服务器相关
+ */
+public class ServerConstants {
+    /**
+     * 图片类型
+     */
+    public static final int TYPE_IMAGE = 1;
+}
